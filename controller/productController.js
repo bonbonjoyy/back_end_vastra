3b5a493ae94d1a9987805e3d2d820b4fd1ce5460
Rizky Gusti <rizkygusti808@gmail.com>
2025-02-01 23:00:22 +0700

    asdfg

--- controller/productController.js
+++ controller/productController.js
@@ -76,23 +76,26 @@ const deleteProduct = async (req, res) => {
 
 const getProductsByCategory = async (req, res) => {
   const { category } = req.params;
+  console.log("Category received:", category); // Debugging
 
   try {
-    console.log("Fetching products for category:", category); // Log kategori untuk debugging
     const products = await Product.findAll({
-      where: { kategori: category },  // Pastikan field 'kategori' sesuai dengan kolom di database
+      where: { kategori: category },  
     });
 
     if (products.length === 0) {
+      console.log(`No products found for category: ${category}`);
       return res.status(404).json({ message: `No products found for category: ${category}` });
     }
 
-    res.status(200).json(products); // Mengembalikan status 200 dan produk yang ditemukan
+    console.log("Products found:", products.length);
+    res.status(200).json(products);
   } catch (error) {
-    console.error("Error fetching products by category:", error); // Log error untuk debugging
+    console.error("Error fetching products by category:", error);
     res.status(500).json({ message: "Error fetching products by category", error: error.message });
   }
 };
+
 
 const getProductById = async (req, res) => {
   const { id } = req.params;
