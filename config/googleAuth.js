2d2c5251c3484edf4ff1cc9d315183be4edd420b
Rizky Gusti <rizkygusti808@gmail.com>
2025-02-12 19:34:28 +0700

    zxc

--- config/googleAuth.js
+++ config/googleAuth.js
@@ -2,8 +2,7 @@ const { OAuth2Client } = require("google-auth-library");
 
 const client = new OAuth2Client(
   process.env.GOOGLE_CLIENT_ID,
-  process.env.GOOGLE_CLIENT_SECRET,
-  process.env.GOOGLE_REDIRECT_URI
+  process.env.GOOGLE_CLIENT_SECRET
 );
 
 module.exports = client;
